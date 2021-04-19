c970e1113c45e741d51ba4d1e580d0a799011fbc
smars-h <matakupresak@gmail.com>
2021-04-19 08:24:29 +0200

    Fix v2

---
+++ commands/say.js
@@ -0,0 +1,15 @@
+const Discord = require('discord.js');
+
+module.exports = {
+    name: 'say',
+    description: 'says what user says',
+    execute(message) {
+        let args = message.content.split(" ").slice(0);
+
+        let say = args.slice(2).join(" ");
+
+        let author = message.author;
+
+        return message.channel.send(`${author} said ${say}`);
+    }
+}
